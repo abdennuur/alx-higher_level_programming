f5b3aba7982f605fcf80625279e757296e503f5f
abdnennuur <abdnourkhatib@gmail.com>
2023-12-03 05:42:02 -0800

    new in list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,7 @@
+#!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx > (len(my_list) - 1):
-        return my_list
-    cp = [ix for ix in my_list]
-    cp[idx] = element
-    return cp
+    if idx < 0 or idx >= len(my_list):
+        return my_list.cp()
+    nw_ls = my_list.cp()
+    nw_ls[idx] = element
+    return nw_ls
