3d0205f49990280d536ed75e3100c35eb1637743
abdnennuur <abdnourkhatib@gmail.com>
2023-12-03 06:43:13 -0800

    print py list info

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,28 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <Python.h>
-
 /**
- * print_python_list_info - print info about python list
- * @p: --PyObject--
- * 
- *Return: (void) 
+ * print_python_list_info - prints basic info about Python lists
+ * @p: PyObject
+ *
+ * Return: not
  */
-
 void print_python_list_info(PyObject *p)
 {
-    long int sz, ix;
-    PyListObject *ls;
-    PyObject *itm;
+PyObject *itm;
+PyListObject *lst = (PyListObject *)p;
+int ix, sz, alc;
 
-    sz = Py_SIZE(p);
-    printf("[*] Size of the Python List = %ld\n", sz);
+sz = Py_SIZE(p);
+alc = lst->allocated;
+printf("[*] Size of the Python List = %d\n", sz);
+printf("[*] Allocated = %d\n", alc);
 
-    ls = (PyListObject *)p;
-    printf("[*] Allocated = %ld\n", ls->allocated);
-
-    for (ix = 0; ix < sz; ix++)
-    {
-        itm = PyList_GetItem(p, ix);
-        printf("Element %ld: %s\n", ix, Py_TYPE(itm)->tp_name);
-    }
+for (ix = 0; ix < sz; ix++)
+{
+itm =  PyList_GetItem(p, ix);
+printf("Element %d: %s\n", ix, Py_TYPE(itm)->tp_name);
+}
 }
