30125cf668fb00e41f77a0130812c6d7b922918d
abdnennuur <abdnourkhatib@gmail.com>
2023-12-03 05:58:46 -0800

    max int

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return (None)
+    bg = my_list[0]
+    for ix in range(len(my_list)):
+        if my_list[i] > bg:
+            bg = my_list[ix]
+    return (bg)
