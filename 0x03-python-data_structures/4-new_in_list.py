3ad0c41374327e9ef622fec9e3d06a87326fa519
abdnennuur <abdnourkhatib@gmail.com>
2023-12-03 05:42:54 -0800

    nw in list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
-        return my_list.cp()
-    nw_ls = my_list.cp()
+        return my_list.copy()
+    nw_ls = my_list.copy()
     nw_ls[idx] = element
     return nw_ls
