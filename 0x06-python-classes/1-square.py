bb59419e7b026087f26dfde2c08261b383be73f0
abdennuur <abdnourkhatib@gmail.com>
2023-12-19 05:55:58 -0800

    1 sqr

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" definition of square class """
+
+
+class Square():
+    """Class representing  square"""
+
+    def __init__(self, size):
+        """ to initialize the class
+
+        Args:
+           size (int): the size of the square
+        """
+        self.__size = size
