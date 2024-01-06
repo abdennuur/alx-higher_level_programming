cd08b51da327010ecc4b0172cfc8943310d7938e
abdennuur <abdnourkhatib@gmail.com>
2024-01-06 08:01:28 -0800

    rec7

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -1,35 +1,37 @@
 #!/usr/bin/python3
-"""
-Defines class Rectangle
-"""
+"""Defines Rectangle class"""
 
 
 class Rectangle:
-    """Representation a rectangle"""
+    """Represent  rectangle.
+
+    Attributes:
+        number_of_instances (int):  number of Rectangle instances.
+        print_symbol (any): symbol used for string representation.
+    """
 
     number_of_instances = 0
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
-        """Initialize the rectangle"""
+        """Initialize  new Rectangle.
+
+        Args:
+            width (int): width of the new rectangle.
+            height (int): height of the new rectangle.
+        """
+        type(self).number_of_instances += 1
         self.width = width
         self.height = height
-        Rectangle.number_of_instances += 1
-
-    def __del__(self):
-        """prints  str when instance has been deleted"""
-        print("Bye rectangle...")
-        Rectangle.number_of_instances -= 1
 
     @property
     def width(self):
-        """getter for private instance attribute width"""
+        """Get/set width of  Rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """setter for  private instance attribute width"""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -37,36 +39,49 @@ class Rectangle:
 
     @property
     def height(self):
-        """getter for  private instance attribute height"""
+        """Get/set  height of  Rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """setter for  private instance attribute height"""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
-        """returns  area of the rectangle"""
-        return self.__width * self.__height
+        """Return  area of t Rectangle."""
+        return (self.__width * self.__height)
 
     def perimeter(self):
-        """returns  perimeter of the rectangle"""
+        """Return  perimeter of the Rectangle."""
         if self.__width == 0 or self.__height == 0:
-            return 0
-        return (self.__width * 2) + (self.__height * 2)
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
-        """returns printable str representation of rectangle"""
-        stri = ""
-        if self.__width != 0 and self.__height != 0:
-            stri += "\n".join(str(self.print_symbol) * self.__width
-                                for ji in range(self.__height))
-        return stri
+        """Return  printable representation of the Rectangle.
+
+        Represent rectangle with the # character.
+        """
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+
+        rec = []
+        for ix in range(self.__height):
+            [rec.append(str(self.print_symbol)) for ji in range(self.__width)]
+            if ix != self.__height - 1:
+                rec.append("\n")
+        return ("".join(rec))
 
     def __repr__(self):
-        """returns a str representation of  rectangle for reproduction"""
-        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+        """Return the str representation of the Rectangle."""
+        rec = "Rectangle(" + str(self.__width)
+        rec += ", " + str(self.__height) + ")"
+        return (rec)
+
+    def __del__(self):
+        """Print  message for every deletion of  Rectangle."""
+        type(self).number_of_instances -= 1
+        print("Bye rectangle...")
