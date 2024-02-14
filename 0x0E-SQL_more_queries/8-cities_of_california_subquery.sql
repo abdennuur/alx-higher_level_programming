aea47c1b625db32938b15237a74a649a3c977280
abdennuur <abdnourkhatib@gmail.com>
2024-02-14 09:06:36 -0800

    mandatory

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+-- To List all cities of CA in database hbtn_0d_usa.
+-- The Results are ordered by ascending cities.id.
+SELECT `id`, `name`
+  FROM `cities`
+ WHERE `state_id` IN
+       (SELECT `id`
+	  FROM `states`
+	 WHERE `name` = "California")
+ ORDER BY `id`;
