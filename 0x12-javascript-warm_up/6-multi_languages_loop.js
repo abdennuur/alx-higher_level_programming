e6a24d074eb467597fc93fbc4e2eac318179719c
abdennuur <abdnourkhatib@gmail.com>
2024-03-11 09:37:47 -0700

    semistandard fix

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-for (const lines of ['C is fun', 'Python is cool', 'JavaScript is amazing'])
-{
+for (const lines of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
   console.log(lines);
 }
