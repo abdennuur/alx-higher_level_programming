d8e29e8a803960ae4f4d2f85ce19f0be11d88b29
abdennuur <abdnourkhatib@gmail.com>
2024-04-23 01:24:43 -0700

    ALL

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+const request = require('request');
+const { argv } = require('process');
+
+const baseUrl = 'https://swapi-api.hbtn.io/api/films/';
+function makeRequest (url) {
+  return new Promise(function (resolve, reject) {
+    request(url, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        resolve(body);
+      } else {
+        reject(error);
+      }
+    });
+  });
+}
+
+async function main() {
+  try {
+    const movieResponse = await makeRequest(baseUrl + argv[2]);
+    const characters = JSON.parse(movieResponse).characters;
+
+    const characterNames = await Promise.all(characters.map(async function (characterUrl) {
+      const characterResponse = await makeRequest(characterUrl);
+      return JSON.parse(characterResponse).name;
+    }));
+
+    characterNames.sort();
+    characterNames.forEach(name => console.log(name));
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+main();
